01af902252e29e4e366526a91c8f712664784b62
stef663k <stef663k@elev.techcollege.dk>
2023-02-17 10:01:01 +0100

    Opgave1.1

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,7 @@ const std = @import("std");
 // fn main() void {
 //     std.debug.print("Hello world!\n", .{});
 // }
+//Here we can see it is missing the pub making it public
  pub fn main() void {
    std.debug.print("Hello {s}\n", .{"World"}); 
 }
