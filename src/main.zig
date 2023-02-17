1243e59c042dfe107293791df465f5900abcb216
stef663k <stef663k@elev.techcollege.dk>
2023-02-17 10:10:50 +0100

    Opgave2

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,3 @@
-const std = @import("std");
-
 //Opgave 1 
 //const std = @import("std");
 
@@ -7,6 +5,16 @@ const std = @import("std");
 //     std.debug.print("Hello world!\n", .{});
 // }
 //Here we can see it is missing the pub making it public
+
+//Opgave 2
+// ??? = @import("std");
+
+// pub fn main() void {
+//     std.debug.print("Standard Library.\n", .{});
+// }
+//Here we can see that we need to set import as a constant
+const std = @import("std");
+
  pub fn main() void {
    std.debug.print("Hello {s}\n", .{"World"}); 
 }
