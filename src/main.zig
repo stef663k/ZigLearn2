e580433614c4d48137a8d5174b1bcf64e4289fd3
stef663k <stef663k@elev.techcollege.dk>
2023-02-17 10:28:50 +0100

    Opgave3

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,9 @@
 //     std.debug.print("Hello world!\n", .{});
 // }
 //Here we can see it is missing the pub making it public
+//  pub fn main() void {
+//    std.debug.print("Hello {s}\n", .{"World"}); 
+// }
 
 //Opgave 2
 // ??? = @import("std");
@@ -13,10 +16,33 @@
 //     std.debug.print("Standard Library.\n", .{});
 // }
 //Here we can see that we need to set import as a constant
+//const std = @import("std");
+
+//Opgave 3
+// const std = @import("std");
+
+// pub fn main() void {
+//     const n: u8 = 50;
+//     n = n + 5;
+
+//     const pi: u8 = 314159;
+
+//     const negative_eleven: u8 = -11;
+
+//     std.debug.print("{} {} {}\n", .{ n, pi, negative_eleven });
+// }
+//We can see that they start chaning a constant, which you cannot, 
+//they then places too high a number in const pi
+//They then assign an assigned integer and making it negative
 const std = @import("std");
 
- pub fn main() void {
-   std.debug.print("Hello {s}\n", .{"World"}); 
+pub fn main() void {
+    var n: u8 = 50;
+    n = n + 5;
+
+    const pi: u32 = 314159;
+
+    const negative_eleven: i8 = -11;
+
+    std.debug.print("{} {} {}\n", .{ n, pi, negative_eleven });
 }
-
-
