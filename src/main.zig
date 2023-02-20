7fe928fdbd6dd12f2d5e273e3286e2ea5208e5df
stef663k <stef663k@elev.techcollege.dk>
2023-02-20 12:29:45 +0100

    Opgave 5

--- src/main.zig
+++ src/main.zig
@@ -78,35 +78,117 @@
 //         first, fourth, length,
 //     });
 // }
-/// We are changing const to var so we can change the value at a later date
-/// We are åutting 3 into the empty space for the fourth value
-/// Finally we are writing len into the last problem to get the lenght of the array
+/// We are changing const to var so we can change the value at a later date.
+/// We are åutting 3 into the empty space for the fourth value.
+/// Finally we are writing len into the last problem,
+/// to get the lenght of the array.
+// const std = @import("std");
+
+// pub fn main() void {
+//     // (Problem 1)
+//     // This "const" is going to cause a problem later - can you see what it is?
+//     // How do we fix it?
+//     var some_primes = [_]u8{ 1, 3, 5, 7, 11, 13, 17, 19 };
+
+//     // Individual values can be set with '[]' notation.
+//     // Example: This line changes the first prime to 2 (which is correct):
+//     some_primes[0] = 2;
+
+//     // Individual values can also be accessed with '[]' notation.
+//     // Example: This line stores the first prime in "first":
+//     const first = some_primes[0];
+
+//     // (Problem 2)
+//     // Looks like we need to complete this expression. Use the example
+//     // above to set "fourth" to the fourth element of the some_primes array:
+//     const fourth = some_primes[3];
+
+//     // (Problem 3)
+//     // Use the len property to get the length of the array:
+//     const length = some_primes.len;
+
+//     std.debug.print("First: {}, Fourth: {}, Length: {}\n", .{
+//         first, fourth, length,
+//     });
+// }
+
+///Opgave 5
+// const std = @import("std");
+
+// pub fn main() void {
+//     const le = [_]u8{ 1, 3 };
+//     const et = [_]u8{ 3, 7 };
+
+//     // (Problem 1)
+//     // Please set this array concatenating the two arrays above.
+//     // It should result in: 1 3 3 7
+//     const leet = ???;
+
+//     // (Problem 2)
+//     // Please set this array using repetition.
+//     // It should result in: 1 0 0 1 1 0 0 1 1 0 0 1
+//     const bit_pattern = [_]u8{ ??? } ** 3;
+
+//     // Okay, that's all of the problems. Let's see the results.
+//     //
+//     // We could print these arrays with leet[0], leet[1],...but let's
+//     // have a little preview of Zig 'for' loops instead:
+//     //
+//     //    for (<item array>) |item| { <do something with item> }
+//     //
+//     // Don't worry, we'll cover looping properly in upcoming
+//     // lessons.
+//     //
+//     std.debug.print("LEET: ", .{});
+
+//     for (leet) |n| {
+//         std.debug.print("{}", .{n});
+//     }
+
+//     std.debug.print(", Bits: ", .{});
+
+//     for (bit_pattern) |n| {
+//         std.debug.print("{}", .{n});
+//     }
+
+//     std.debug.print("\n", .{});
 const std = @import("std");
 
 pub fn main() void {
+    const le = [_]u8{ 1, 3 };
+    const et = [_]u8{ 3, 7 };
+
     // (Problem 1)
-    // This "const" is going to cause a problem later - can you see what it is?
-    // How do we fix it?
-    var some_primes = [_]u8{ 1, 3, 5, 7, 11, 13, 17, 19 };
-
-    // Individual values can be set with '[]' notation.
-    // Example: This line changes the first prime to 2 (which is correct):
-    some_primes[0] = 2;
-
-    // Individual values can also be accessed with '[]' notation.
-    // Example: This line stores the first prime in "first":
-    const first = some_primes[0];
+    // Please set this array concatenating the two arrays above.
+    // It should result in: 1 3 3 7
+    const leet = le ++ et ++ [_]u8{4};
 
     // (Problem 2)
-    // Looks like we need to complete this expression. Use the example
-    // above to set "fourth" to the fourth element of the some_primes array:
-    const fourth = some_primes[3];
+    // Please set this array using repetition.
+    // It should result in: 1 0 0 1 1 0 0 1 1 0 0 1
+    const bit_pattern = [_]u8{ 1,0,0,1 } ** 3;
 
-    // (Problem 3)
-    // Use the len property to get the length of the array:
-    const length = some_primes.len;
+    // Okay, that's all of the problems. Let's see the results.
+    //
+    // We could print these arrays with leet[0], leet[1],...but let's
+    // have a little preview of Zig 'for' loops instead:
+    //
+    //    for (<item array>) |item| { <do something with item> }
+    //
+    // Don't worry, we'll cover looping properly in upcoming
+    // lessons.
+    //
+    std.debug.print("LEET: ", .{});
 
-    std.debug.print("First: {}, Fourth: {}, Length: {}\n", .{
-        first, fourth, length,
-    });
+    for (leet) |n| {
+        std.debug.print("{}", .{n});
+    }
+
+    std.debug.print(", Bits: ", .{});
+
+    for (bit_pattern) |n| {
+        std.debug.print("{}", .{n});
+    }
+
+    std.debug.print("\n", .{});
 }
