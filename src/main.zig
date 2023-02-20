9d511171d72ff5cb972a2e96c7e2703abb2a60fc
stef663k <stef663k@elev.techcollege.dk>
2023-02-20 10:24:44 +0100

    Comments

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,24 @@
-//Opgave 1 
+///Opgave 1 
 //const std = @import("std");
 
 // fn main() void {
 //     std.debug.print("Hello world!\n", .{});
 // }
-//Here we can see it is missing the pub making it public
+///Here we can see it is missing the pub making it public
 //  pub fn main() void {
 //    std.debug.print("Hello {s}\n", .{"World"}); 
 // }
 
-//Opgave 2
+///Opgave 2
 // ??? = @import("std");
 
 // pub fn main() void {
 //     std.debug.print("Standard Library.\n", .{});
 // }
-//Here we can see that we need to set import as a constant
-//const std = @import("std");
+///Here we can see that we need to set import as a constant
+///const std = @import("std");
 
-//Opgave 3
+///Opgave 3
 // const std = @import("std");
 
 // pub fn main() void {
@@ -31,10 +31,10 @@
 
 //     std.debug.print("{} {} {}\n", .{ n, pi, negative_eleven });
 // }
-//We can see that they start changing a constant, which you cannot, 
-//they then places too high a number in const pi
-//They then assign an assigned integer and making it negative
-//Pi was also not a decimal or in this case a float
+///We can see that they start changing a constant, which you cannot, 
+///they then places too high a number in const pi
+///They then assign an assigned integer and making it negative
+///Pi was also not a decimal or in this case a float
 const std = @import("std");
 
 pub fn main() void {
