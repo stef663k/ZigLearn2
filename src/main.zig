4ffa9dc203df60c1df5e48b5754e7acbd084d98f
stef663k <stef663k@elev.techcollege.dk>
2023-02-21 12:25:10 +0100

    Opgave 9

--- src/main.zig
+++ src/main.zig
@@ -325,38 +325,65 @@
 //     // We want to "Program in Zig!" of course:
 //     std.debug.print("Program in {s}!\n", .{lang});
 // }
+// const std = @import("std");
+
+// pub fn main() void {
+//     // What is this nonsense? :-)
+//     const letters = "YZhifg";
+
+//     // Note: usize is an unsigned integer type used for...sizes.
+//     // The exact size of usize depends on the target CPU
+//     // architecture. We could have used a u8 here, but usize is
+//     // the idiomatic type to use for array indexing.
+//     //
+//     // There IS a problem on this line, but 'usize' isn't it.
+//     var x: usize = 1;
+
+//     // Note: When you want to declare memory (an array in this
+//     // case) without putting anything in it, you can set it to
+//     // 'undefined'. There is no problem on this line.
+//     var lang: [3]u8 = undefined;
+
+//     // The following lines attempt to put 'Z', 'i', and 'g' into the
+//     // 'lang' array we just created by indexing the array
+//     // 'letters' with the variable 'x'. As you can see above, x=1
+//     // to begin with.
+//     lang[0] = letters[x];
+
+//     x = 3;
+//     lang[1] = letters[x];
+
+//     x = 5;
+//     lang[2] = letters[x];
+
+//     // We want to "Program in Zig!" of course:
+//     std.debug.print("Program in {s}!\n", .{lang});
+// }
+
+///Opgave 9
+// const std = @import("std");
+
+// pub fn main() void {
+//     const foo = 1;
+
+//     // Please fix this condition:
+//     if (foo) {
+//         // We want our program to print this message!
+//         std.debug.print("Foo is 1!\n", .{});
+//     } else {
+//         std.debug.print("Foo is not 1!\n", .{});
+//     }
+// }
 const std = @import("std");
 
 pub fn main() void {
-    // What is this nonsense? :-)
-    const letters = "YZhifg";
+    const foo = 1;
 
-    // Note: usize is an unsigned integer type used for...sizes.
-    // The exact size of usize depends on the target CPU
-    // architecture. We could have used a u8 here, but usize is
-    // the idiomatic type to use for array indexing.
-    //
-    // There IS a problem on this line, but 'usize' isn't it.
-    var x: usize = 1;
-
-    // Note: When you want to declare memory (an array in this
-    // case) without putting anything in it, you can set it to
-    // 'undefined'. There is no problem on this line.
-    var lang: [3]u8 = undefined;
-
-    // The following lines attempt to put 'Z', 'i', and 'g' into the
-    // 'lang' array we just created by indexing the array
-    // 'letters' with the variable 'x'. As you can see above, x=1
-    // to begin with.
-    lang[0] = letters[x];
-
-    x = 3;
-    lang[1] = letters[x];
-
-    x = 5;
-    lang[2] = letters[x];
-
-    // We want to "Program in Zig!" of course:
-    std.debug.print("Program in {s}!\n", .{lang});
+    // Please fix this condition:
+    if (foo == 1) {
+        // We want our program to print this message!
+        std.debug.print("Foo is 1!\n", .{});
+    } else {
+        std.debug.print("Foo is not 1!\n", .{});
+    }
 }
-
