daf26c61b37072f3724107023a605e97b6dc595e
stef663k <stef663k@elev.techcollege.dk>
2023-02-28 09:39:51 +0100

    Opgave 11

--- src/main.zig
+++ src/main.zig
@@ -424,14 +424,67 @@
 //     std.debug.print("With the discount, the price is ${}.\n", .{price});
 // }
 
+// const std = @import("std");
+
+// pub fn main() void {
+//     var discount = true;
+
+//     // Please use an if...else expression to set "price".
+//     // If discount is true, the price should be $17, otherwise $20:
+//     var price: u8 = if (discount) 17 else 20;
+
+//     std.debug.print("With the discount, the price is ${}.\n", .{price});
+// }
+
+///Opgave 11
+//
+// Zig 'while' statements create a loop that runs while the
+// condition is true. This runs once (at most):
+//
+//     while (condition) {
+//         condition = false;
+//     }
+//
+// Remember that the condition must be a boolean value and
+// that we can get a boolean value from conditional operators
+// such as:
+//
+//     a == b   means "a equals b"
+//     a < b    means "a is less than b"
+//     a > b    means "a is greater than b"
+//     a != b   means "a does not equal b"
+//
+// const std = @import("std");
+
+// pub fn main() void {
+//     var n: u32 = 2;
+
+//     // Please use a condition that is true UNTIL "n" reaches 1024:
+//     while (???) {
+//         // Print the current number
+//         std.debug.print("{} ", .{n});
+
+//         // Set n to n multiplied by 2
+//         n *= 2;
+//     }
+
+//     // Once the above is correct, this will print "n=1024"
+//     std.debug.print("n={}\n", .{n});
+// }
 const std = @import("std");
 
 pub fn main() void {
-    var discount = true;
+    var n: u32 = 2;
 
-    // Please use an if...else expression to set "price".
-    // If discount is true, the price should be $17, otherwise $20:
-    var price: u8 = if (discount) 17 else 20;
+    // Please use a condition that is true UNTIL "n" reaches 1024:
+    while (n < 1024) {
+        // Print the current number
+        std.debug.print("{} ", .{n});
 
-    std.debug.print("With the discount, the price is ${}.\n", .{price});
+        // Set n to n multiplied by 2
+        n *= 2;
+    }
+
+    // Once the above is correct, this will print "n=1024"
+    std.debug.print("n={}\n", .{n});
 }
